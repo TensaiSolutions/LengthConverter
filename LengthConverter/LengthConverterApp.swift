fa526cf102c56ca788d3add2d3a4725e5af52321
Tensai Solutions <sidellp@gmail.com>
2023-10-10 10:23:20 -0400

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,18 @@
+//
+// LengthConverterApp.swift
+// LengthConverter
+//
+// Created by Tensai Solutions on 10/10/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
