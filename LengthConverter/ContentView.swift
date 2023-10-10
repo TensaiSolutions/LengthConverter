5f6faefb6d46023b8e2a6d5c31fe96e680bfb1d0
Tensai Solutions <sidellp@gmail.com>
2023-10-10 11:29:20 -0400

    Initial Solution

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -9,14 +9,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputUnits = 0.0
+    @State private var userInputUnit = "Meters"
+    @State private var userOutputUnit = "Meters"
+    @FocusState private var unitIsFocused: Bool
+    let units = ["Meters", "KM", "Feet", "Yards", "Miles"]
+    var computedDistance: Double {
+        return convertUnits(input: inputUnits, from: userInputUnit, to: userOutputUnit)
+    }
+
+    func convertUnits(input:Double, from unitFrom: String, to unitTo: String ) -> Double {
+        var baseInput = 0.0 //Base Unit amount to base calculations on
+        switch unitFrom {
+        case "Meters":
+            baseInput = input * 1_000
+        case "KM":
+            baseInput = input * 1_000_000
+        case "Feet":
+            baseInput = input * 304.8
+        case "Yards":
+            baseInput = input * 914.4
+        case "Miles":
+            baseInput = input * 1_609_000
+        default:
+            break
         }
-        .padding()
+
+        switch unitTo {
+        case "Meters":
+            return baseInput / 1_000
+        case "KM":
+            return baseInput / 1_000_000
+        case "Feet":
+            return baseInput / 304.8
+        case "Yards":
+            return baseInput / 914.4
+        case "Miles":
+            return baseInput / 1_609_000
+        default:
+            return 0.0
+        }
+    }
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Enter a Unit Amount") {
+                    TextField("Units", value: $inputUnits, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($unitIsFocused)
+                }
+                Section("Select Unit of Measure to Convert From") {
+                    Picker("Units From", selection: $userInputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Select Unit of Measure to Convert To") {
+                    Picker("Units From", selection: $userOutputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Result of Converting \(userInputUnit) to \(userOutputUnit)") {
+                    Text("\(computedDistance.formatted())")
+                }
+            }
+        }
     }
 }
 
